9ff2c08aba330b363dc60c44d747f85d717d606b
venky <venkateshnarayanan@yahoo.com>
2017-07-18 17:30:36 +0530

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package website.photodirect.photodirect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
